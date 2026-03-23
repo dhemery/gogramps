43ee47753503259cc01871ab0025ba9ce70a1fd2
Dale Emery <dale@dhemery.com>
2026-03-22 21:25:01 -0700

    Add person gender (as int for now)

--- gen/person.go
+++ gen/person.go
@@ -2,6 +2,7 @@ package gen
 
 type Person struct {
 	GrampsObject
+	Gender           int              `json:"gender"`
 	Citations        []CitationHandle `json:"citation_list"`
 	FamiliesAsParent []FamilyHandle   `json:"family_list"`
 	FamiliesAsChild  []FamilyHandle   `json:"parent_family_list"`
