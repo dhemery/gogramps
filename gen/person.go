193b304378e4a06b24703a6282a83a8a510df317
Dale Emery <dale@dhemery.com>
2026-03-23 21:49:27 -0700

    Add surname origin and json omission tags

--- gen/person.go
+++ gen/person.go
@@ -3,7 +3,7 @@ package gen
 type Person struct {
 	GrampsObject     `json:",inline"`
 	Name             PersonName       `json:"primary_name"`
-	AlternateNames   []PersonName     `json:"alternate_names"`
+	AlternateNames   []PersonName     `json:"alternate_names,omitempty"`
 	Gender           int              `json:"gender"`
 	FamiliesAsParent []FamilyHandle   `json:"family_list,omitempty"`
 	FamiliesAsChild  []FamilyHandle   `json:"parent_family_list,omitempty"`
@@ -16,23 +16,31 @@ type Person struct {
 }
 
 type PersonName struct {
-	Private   bool             `json:"private"`
-	Title     string           `json:"title"`
-	Given     string           `json:"first_name"`
+	Private   bool             `json:"private,omitzero"`
+	Title     string           `json:"title,omitzero"`
+	Given     string           `json:"first_name,omitzero"`
 	Surnames  []Surname        `json:"surname_list"`
-	Date      Date             `json:"date"`
-	Call      string           `json:"call"`
-	FamNick   string           `json:"famnick"`
-	Nick      string           `json:"nick"`
-	DisplayAs int              `json:"display_as"`
-	GroupAs   string           `json:"group_as"`
-	SortAs    int              `json:"sort_as"`
-	Citations []CitationHandle `json:"citation_list"`
-	Notes     []Note           `json:"note_list"`
+	Date      Date             `json:"date,omitzero"`
+	Call      string           `json:"call,omitzero"`
+	FamNick   string           `json:"famnick,omitzero"`
+	Nick      string           `json:"nick,omitzero"`
+	DisplayAs int              `json:"display_as,omitzero"`
+	GroupAs   string           `json:"group_as,omitzero"`
+	SortAs    int              `json:"sort_as,omitzero"`
+	Citations []CitationHandle `json:"citation_list,omitempty"`
+	Notes     []Note           `json:"note_list,omitempty"`
 }
 
 type Surname struct {
-	Connector string `json:"connector"`
-	Prefix    string `json:"prefix"`
-	Surname   string `json:"surname"`
+	Connector  string     `json:"connector,omitzero"`
+	Prefix     string     `json:"prefix,omitzero"`
+	Surname    string     `json:"surname,omitzero"`
+	OriginType OriginType `json:"origin_type,omitzero"`
+}
+
+type OriginTypeCode int
+
+type OriginType struct {
+	Code   OriginTypeCode `json:"value,omitzero"`
+	Custom string         `json:"string,omitempty"`
 }
