9efbe6be159a685f7740ceaa73853ec912ac5eb6
Dale Emery <dale@dhemery.com>
2026-03-23 21:33:47 -0700

    Add primary and alternate names to Person

--- gen/person.go
+++ gen/person.go
@@ -2,6 +2,8 @@ package gen
 
 type Person struct {
 	GrampsObject     `json:",inline"`
+	Name             PersonName       `json:"primary_name"`
+	AlternateNames   []PersonName     `json:"alternate_names"`
 	Gender           int              `json:"gender"`
 	FamiliesAsParent []FamilyHandle   `json:"family_list,omitempty"`
 	FamiliesAsChild  []FamilyHandle   `json:"parent_family_list,omitempty"`
@@ -11,4 +13,26 @@ type Person struct {
 	Media            []MediaRef       `json:"media_list,omitempty"`
 	Notes            []NoteHandle     `json:"note_list,omitempty"`
 	Tags             []TagHandle      `json:"tag_list,omitempty"`
+}
+
+type PersonName struct {
+	Private   bool             `json:"private"`
+	Title     string           `json:"title"`
+	Given     string           `json:"first_name"`
+	Surnames  []Surname        `json:"surname_list"`
+	Date      Date             `json:"date"`
+	Call      string           `json:"call"`
+	FamNick   string           `json:"famnick"`
+	Nick      string           `json:"nick"`
+	DisplayAs int              `json:"display_as"`
+	GroupAs   string           `json:"group_as"`
+	SortAs    int              `json:"sort_as"`
+	Citations []CitationHandle `json:"citation_list"`
+	Notes     []Note           `json:"note_list"`
+}
+
+type Surname struct {
+	Connector string `json:"connector"`
+	Prefix    string `json:"prefix"`
+	Surname   string `json:"surname"`
 }
